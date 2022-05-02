884537f045c3b841f3958a93b8762d0c27ad9443
Rebecca Foltin <rebecca.s.foltin@gmail.com>
2022-05-02 13:03:51 -0400

    final deployment

--- curl-scripts/auth/change-password.sh
+++
@@ -1,9 +0,0 @@
-# VARIABLE=VALUE sh curl-scripts/auth/change-password.sh
-
-curl "https://library-express-api.herokuapp.com/change-password" \
-  --include \
-  --request PATCH \
-  --header "Content-Type: application/json" \
-  --data ''
-
-echo
